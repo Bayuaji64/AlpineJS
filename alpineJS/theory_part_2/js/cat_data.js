58b3e4ccdbb17f879fae34023c7b3f50da4714cf
Bayuaji64 <bayuajipradipta64@gmail.com>
2024-02-28 12:48:37 +0700

    X-init and x-for

---
+++ alpineJS/theory_part_2/js/cat_data.js
@@ -0,0 +1,20 @@
+let cat_1 = [
+    {
+        title : "Description",
+        body: "<div class='flex'><img class='mr-2' src='https://placekitten.com/g/150/150'><span>Super nice cat!</span></div>"
+    },
+    {
+        title : "Specifics",
+        body: "<b>Weight:</b> 3kg"
+    },
+]
+let cat_2 = [
+    {
+        title : "Description",
+        body: "<div class='flex'><img class='mr-2' src='https://placekitten.com/g/200/150'><span>Super Beautiful cat!</span></div>"
+    },
+    {
+        title : "Specifics",
+        body: "<b>Weight:</b> 3kg"
+    },
+]
